7fb1a4aafd74cc22e64e254e7a39bbd0dcf3cbd8
irahatam <matahari@nyu.edu>
2020-12-15 18:24:40 -0500

    boilerplate

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+// Backend Application for Final Project
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Backend is running at port:${port}`));
